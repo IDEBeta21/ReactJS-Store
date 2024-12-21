43f050360a510c1fb947a029483e8df9f51a5fc2
Ian Deladia (Ermino) <kitsmype416@gmail.com>
2024-12-21 13:54:12 +0800

    Revert "Updates"
    This reverts commit e4d839b48e6c5a8d7972af182e28681cf2db915c.

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import './Dashboard.scss'
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-page">
+      <div>Dashboard - Page</div>
+    </div>
+  )
+}
+
+export default Dashboard
